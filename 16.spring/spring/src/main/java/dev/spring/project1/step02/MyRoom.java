79c52cb7c6a60841f1bc43361bd43d916b8a453c
j0303p <j0303p@gmail.com>
2024-09-02 10:52:42 +0900

    feat: .

--- 16.spring/spring/src/main/java/dev/spring/project1/step02/MyRoom.java
+++ 16.spring/spring/src/main/java/dev/spring/project1/step02/MyRoom.java
@@ -1,6 +1,5 @@
 package dev.spring.project1.step02;
 
-import dev.spring.project1.step01.factory.Tape;
 import dev.spring.project1.step01.factory.TapeReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -8,10 +7,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyRoom {
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("setter-config.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("constructor-config.xml");
+
         TapeReader reader=context.getBean(TapeReader.class);
-        System.out.println(reader);
-        Tape tape = context.getBean(Tape.class);
-        System.out.println(tape);
+        reader.test();
     }
 }
