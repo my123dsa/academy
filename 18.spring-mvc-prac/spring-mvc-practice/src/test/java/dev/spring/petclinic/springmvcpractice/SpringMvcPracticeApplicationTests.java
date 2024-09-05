080029f0bdbcda04d470bf89e077e0f1b9a46e8a
j0303p <j0303p@gmail.com>
2024-09-05 10:00:51 +0900

    feat: change

---
+++ 18.spring-mvc-prac/spring-mvc-practice/src/test/java/dev/spring/petclinic/springmvcpractice/SpringMvcPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.spring.petclinic.springmvcpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
